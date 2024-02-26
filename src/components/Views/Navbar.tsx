1acb17a9db3dbc6c410186533731b2e1bafc791f
Wasif <wk5599426@gmail.com>
2024-02-26 16:20:09 +0500

    edited

--- src/components/Views/Navbar.tsx
+++ src/components/Views/Navbar.tsx
@@ -1,14 +1,13 @@
-import React from 'react'
-import { Button, buttonVariants } from "@/components/ui/button"
-import Link from 'next/link'
-import Image from 'next/image'
-import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-import { SearchIcon, ShoppingCart } from 'lucide-react';
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
-import { DropdownMenuDemo } from '../ui/Avatardispaly';
+import Image from "next/image";
+import Link from "next/link";
+import { Search } from 'lucide-react';
+import { ShoppingCart } from 'lucide-react';
+import { LoginLink, RegisterLink, getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { buttonVariants } from "../ui/button";
+import { DropdownMenuDemo } from "../ui/Avatardispaly";
 
 interface navArray {
-    label: string
+    label: string,
     href: string
 }
 
@@ -31,41 +30,40 @@ const navArray: navArray[] = [
     },
 ]
 
-
 const Navbar = () => {
     const {getUser} = getKindeServerSession();
     const user = getUser();
-    console.log('user: ', user);
   return (
     <header className="text-gray-600 body-font">
-  <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
-    <Link className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href={'/'}>
+    <div className="container mx-auto flex flex-wrap flex-col md:flex-row items-center">
+      <Link href="/" className="flex title-font font-medium items-center mb-4 md:mb-0">
         <Image src={"/STC.png"} width={120} height={100} alt="Saras trading co. STC rice, spices, pulses, dry fruits"/>
-    </Link>
-    <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center justify-center gap-2">
+      </Link>
+      <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center justify-center gap-2">
         {navArray.map((item, index)=>(
              // eslint-disable-next-line react/jsx-key
-             <Link href={item.href} className="cursor-pointer hover:underline mr-5 hover:text-[#3BB77E] text-[#504E4E] font-medium text-lg duration-300">{item.label}</Link>
+             <Link key={index} href={item.href} className="cursor-pointer hover:underline mr-5 hover:text-[#3BB77E] text-[#504E4E] font-semibold text-lg duration-300">{item.label}</Link>
         ))}
       </nav>
-    <div className='space-x-3 flex items-center'>
-    <ShoppingCart />
-    <SearchIcon/>
-        {!user ? (<div><LoginLink className={buttonVariants({
-            variant:'outline',
-            size:'sm'
-        })}>Sign in</LoginLink>
-
-        <RegisterLink className={buttonVariants({
-            variant:'secondary',
-            size:'sm'
-        })}>Sign up</RegisterLink></div>):(
-            <DropdownMenuDemo/>
+      <div className="flex space-x-4 items-center">
+        <div className="flex items-center space-x-3">
+        <Search size={23}/>
+        <ShoppingCart size={23}/>
+        </div>
+        {!user ? (<div>
+            <LoginLink className={buttonVariants({variant: 'outline', size: 'sm'})} style={{outline:'#3BB77E'}}>
+                Sign in
+            </LoginLink>
+            <RegisterLink className={buttonVariants({variant: 'secondary',size: 'sm'})} style={{ backgroundColor: '#3BB77E', color: 'white' }}>
+                Sign up
+            </RegisterLink>
+        </div>):(
+          <DropdownMenuDemo UserData={user}/>
         )}
+      </div>
     </div>
-  </div>
-</header>
-  )
-}
+  </header>
+  );
+};
 
-export default Navbar
+export default Navbar; 
--- src/components/ui/Avatardispaly.tsx
+++ src/components/ui/Avatardispaly.tsx
@@ -1,25 +1,63 @@
-
-import {DropdownMenu,DropdownMenuContent,DropdownMenuItem,DropdownMenuLabel,DropdownMenuSeparator,DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
-import { Avatar,AvatarFallback,AvatarImage,} from "@/components/ui/avatar"
-import Link from "next/link"
-
-export function DropdownMenuDemo() {
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-            <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56">
-        <DropdownMenuLabel className="text-center text-[#3BB77E]">My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <Link target="_blank" href={'/'}><DropdownMenuItem>About</DropdownMenuItem></Link>
-        <Link target="_blank" href={'/'}><DropdownMenuItem>Contact</DropdownMenuItem></Link>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem  className="hover:bg-[#3BB77E] hover:text-white">Log out</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  )
-};
+import {
+    Github,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    User
+  } from "lucide-react"
+  
+  import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+  } from "@/components/ui/avatar"
+  import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger
+  } from "@/components/ui/dropdown-menu"
+  import Link from "next/link"
+  import { KindeUser, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/server"
+  
+  export function DropdownMenuDemo({UserData}:{UserData:KindeUser}) {
+  
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56 bg-white">
+                <DropdownMenuLabel className="text-center text-[#3BB77E]">My Account</DropdownMenuLabel>
+                <DropdownMenuItem className="hover:bg-[#3BB77E] hover:text-white">
+                    <User className="mr-2 h-4 w-4" />
+                    <span>{UserData.given_name}{" "}{UserData.family_name}</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="hover:bg-[#3BB77E] hover:text-white">
+                    <Mail className="mr-2 h-4 w-4" />
+                    <span>{UserData.email}</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="hover:bg-[#3BB77E] hover:text-white">
+                    <Github className="mr-2 h-4 w-4" />
+                    <Link target="_blank" href={"    "}>GitHub</Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="hover:bg-[#3BB77E] hover:text-white">
+                    <LifeBuoy className="mr-2 h-4 w-4" />
+                    <Link target="_blank" href={"     "}>Support</Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="hover:bg-[#3BB77E] hover:text-white">
+                    <LogOut className="mr-2 h-4 w-4" />
+                    <LogoutLink>Log out</LogoutLink>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+  }
+  
+  
